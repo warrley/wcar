b14f5243be50f46c5c2a25fb08513c7c26d7a6c0
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-06 15:14:50 -0300

    userCars -> outside CarContext

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,45 @@
 import { PanelHeader } from "../components/panel-header"
-import { useCar } from "../context/CarContex"
 import { CarDashboard } from "../components/CarDasboard";
+import { useEffect, useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { CarsProps } from "../context/CarContex";
+import { useAuth } from "../context/AuthContext";
+import { db } from "../services/firebase";
 
 export const Dasboard = () => {
-  const { cars } = useCar();
+  const [cars, setCars] = useState<CarsProps[]>([]);
+  const { user } = useAuth()
+
+  useEffect(() => {
+    const loadCars = async () => {
+      if (!user?.uid) return;
+
+      const carsRef = collection(db, "cars");
+      const queryRef = query(carsRef, where("uid", "==", user.uid));
+
+      getDocs(queryRef)
+        .then((snapshot) => {
+          let carList = [] as CarsProps[];
+          snapshot.forEach(doc => {
+            carList.push({
+              id: doc.id,
+              name: doc.data().name,
+              year: doc.data().year,
+              km: doc.data().km,
+              city: doc.data().city,
+              price: doc.data().price,
+              images: doc.data().images,
+              uid: doc.data().uid
+            })
+          })
+
+          setCars(carList);
+        })
+    };
+
+    loadCars();
+
+  },[user])
 
   return (
     <div>
