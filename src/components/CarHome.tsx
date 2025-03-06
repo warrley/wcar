bd326234504f6867589cf93ba61e24ed5661d6f3
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-06 11:24:41 -0300

    skeleton img

--- src/components/CarHome.tsx
+++ src/components/CarHome.tsx
@@ -1,6 +1,7 @@
 import { Circle } from "lucide-react"
 import { ImageProps } from "../pages/home";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 interface CarProps{
   name: string;
@@ -13,26 +14,33 @@ interface CarProps{
 }
 
 export const CarHome = ({ name, id, year, km, city, price, images }: CarProps) => {
-    return (
-      <Link to={`/car/:${id}`}>
-        <section className="bg-white rounded-lg flex flex-col hover:scale-110 duration-300">
-          <img className="rounded-lg h-80 object-cover" src={images[0].url} alt={name} />
+  const [loadImg, setLoadImg] = useState<string[]>([]);
 
-          <div className="p-3">
-          <p className="font-bold text-xl">{name}</p>
+  const handleImageLoad = (id: string) => {
+    setLoadImg((prevImg) => [...prevImg, id]);
+  }
 
-            <div className="flex items-center gap-1 mb-4">
-            <p>{year}</p>
-                <Circle size={5} fill="currentColor"/>
-            <p>{km}</p>
-            </div>
+  return (
+    <Link to={`/car/:${id}`}>
+      <section className="bg-white rounded-lg flex flex-col hover:scale-110 duration-300">
+        <div className="w-full h-80 rounded-lg bg-slate-200" style={{ display: loadImg.includes(id) ? "none" : "block"}}></div>
+        <img className="rounded-lg h-80 object-cover" style={{ display: loadImg.includes(id) ? "block" : "none"}} src={images[0].url} alt={name} onLoad={() => handleImageLoad(id)}/>
 
-          <h2 className="text-xl font-semibold">
-            R$ {price}
-          </h2>
-        </div>
-        <p className="p-2 border-t">{city}</p>
-        </section>
-      </Link>
-    )
+        <div className="p-3">
+        <p className="font-bold text-xl">{name}</p>
+
+          <div className="flex items-center gap-1 mb-4">
+          <p>{year}</p>
+              <Circle size={5} fill="currentColor"/>
+          <p>{km}</p>
+          </div>
+
+        <h2 className="text-xl font-semibold">
+          R$ {price}
+        </h2>
+      </div>
+      <p className="p-2 border-t">{city}</p>
+      </section>
+    </Link>
+  )
 }
