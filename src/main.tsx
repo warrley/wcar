929cded6d091b9f5cfbd0a858040419b0429521e
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-06 21:21:44 -0300

    fix import swiper/css

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ register();
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import 'swiper/css/scrollbar'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
