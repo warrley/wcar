328efce99369c0bf1a344f18d1d0ad34d9b1302a
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-07 20:06:49 -0300

    by

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,8 +5,11 @@ export const Layout = () => {
   return (
     <div className="select-none">
       <Header />
-      <div className=" w-full max-w-7xl px-4 mx-auto">
+      <div className="w-full max-w-7xl px-4 mx-auto">
         <Outlet />
+      </div>
+      <div className="w-full max-w-7xl px-4 mx-auto border-t mt-5">
+          <h1 className="text-sky-500/60 text-center"><a href="https://github.com/warrley" target="_blank">by warleyfarias</a></h1>
       </div>
     </div>
   )
