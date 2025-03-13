83d4bea8807d3360757236bc2288f5fa8cd06b59
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-12 22:59:55 -0300

    footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,7 +8,7 @@ export const Layout = () => {
       <div className="w-full max-w-7xl px-4 mx-auto">
         <Outlet />
       </div>
-      <div className="w-full max-w-7xl px-4 mx-auto border-t mt-5 fixed bottom-0 p-2">
+      <div className="w-full max-w-7xl px-4 mx-auto border-t mt-5  bottom-0 p-2">
         <h1 className="text-sky-500/60 text-center text-sm"><a href="https://github.com/warrley" target="_blank">by warleyfarias</a></h1>
       </div>
     </div>
